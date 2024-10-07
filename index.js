bf3499b0e5d85c96cca5c9b7d1c33f4e1866e8c4
Joy <joeyygatimu@gmail.com>
2024-10-07 10:04:37 +0300

    vivo

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 100) + 1; 
+let num3 = 34;
+let num4 = 6;
+let mod = num3 % num4; 
+let max = Math.max(5, 10, 15, 20); 
+console.log('Multiply:', multiply); 
+console.log('Random:', random);
+console.log('Mod:', mod); 
+console.log('Max:', max); 
+
